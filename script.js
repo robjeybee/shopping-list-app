8686f81f46a73a83af09cd261e4c5369001eb457
Rob B <rob@rjrobbie.dev>
2024-02-08 21:03:21 +0000

    added boilerplate files for project

---
+++ script.js
@@ -0,0 +1,6 @@
+const input = document.getElementById("input")
+const button = document.getElementById("button")
+
+button.addEventListener("click", function () {
+    console.log("Button clicked")
+})
