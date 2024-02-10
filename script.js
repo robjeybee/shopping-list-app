4b92a575d75afbc85bdf70abd2d51189cb559774
Rob B <rob@rjrobbie.dev>
2024-02-10 23:28:16 +0000

    hooked frontend up to Firebase

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-const input = document.getElementById("input")
-const button = document.getElementById("button")
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js"
 
-button.addEventListener("click", function () {
-    console.log("Button clicked")
-})
+const firebaseConfig = {
+  databaseURL: "https://playground-8f4d8-default-rtdb.europe-west1.firebasedatabase.app/"
+};
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app)
+const itemsInDb = ref(database, "items")
+
+const inputEl = document.getElementById("input-el");
+const buttonEl = document.getElementById("button-el");
+
+buttonEl.addEventListener("click", function () {
+  let inputValue = inputEl.value;
+  push(itemsInDb, inputValue)
+  console.log(`${inputValue} added to database`);
+});
+
