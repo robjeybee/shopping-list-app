5cfae64726a9c78db2948bbc0764f79d29920c38
Rob B <rob@rjrobbie.dev>
2024-02-11 21:27:56 +0000

    input values added to the database are now rendered in the innerHTML of a listEl div

--- script.js
+++ script.js
@@ -11,10 +11,24 @@ const itemsInDb = ref(database, "items")
 
 const inputEl = document.getElementById("input-el");
 const buttonEl = document.getElementById("button-el");
+const listEl = document.getElementById("list-el")
 
 buttonEl.addEventListener("click", function () {
   let inputValue = inputEl.value;
   push(itemsInDb, inputValue)
   console.log(`${inputValue} added to database`);
+
+  clearInputEl()
+  addItemToListEl(inputValue)
 });
+
+function clearInputEl() {
+  inputEl.value = '';
+}
+
+function addItemToListEl(itemValue) {
+  listEl.innerHTML += `${itemValue}`
+}
+
+
 
